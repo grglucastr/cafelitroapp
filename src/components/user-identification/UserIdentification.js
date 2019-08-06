da141d66d11d5b34cd5be89478b54ea09c8d385f
George Bentes <george.bentes@gmail.com>
2019-08-06 00:30:12 -0400

    Add hidden input text fieldto usb keyboard badge reader fill it

--- src/components/user-identification/UserIdentification.js
+++ src/components/user-identification/UserIdentification.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
-import { View, Text, Image, Dimensions, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, Dimensions, KeyboardAvoidingView, StyleSheet, Keyboard, TextInput } from 'react-native';
 import { Input, Button } from 'react-native-ui-kitten';
+
+const hideKeyboard = () =>{
+  Keyboard.dismiss;
+}
 
 const UserIdentification = () =>{
 
   const [userNo, setUserNo] = useState("");
-  const window = Dimensions.get('window');
+  const window = Dimensions.get('window');  
 
   return(
     <KeyboardAvoidingView style={{flex:1, justifyContent:'space-around'}}>
@@ -33,15 +37,17 @@ const UserIdentification = () =>{
           source={require('../../imgs/badge.png')}
           resizeMode="contain"
           style={{alignSelf: 'center', height: 130}}
-        />      
-        <Text style={{textAlign: 'center', color: '#fff3c9', fontSize: 20, marginTop: 40, marginBottom:10}}>
+        />     
+
+        <TextInput autoFocus={true} onFocus={Keyboard.dismiss} />
+        <Text style={{textAlign: 'center', color: '#fff3c9', fontSize: 20}}>
           Aproxime o crachá do leitor...
         </Text>
         <View style={{flexDirection: 'row'}} >
           <Input
             value={userNo}
             onChangeText={(string) => setUserNo(string)}
-            keyboardType="number-pad"
+            keyboardType="number-pad"            
             style={{flex: 2, borderRadius:0}}
             placeholder="Ou digite sua matrícula aqui!"
           />
