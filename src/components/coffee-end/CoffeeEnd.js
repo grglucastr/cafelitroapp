067dec97cfa53b8ea07303f420180f1cbd553c3d
George Bentes <george.bentes@gmail.com>
2019-08-15 13:22:52 -0400

    Add android backhandler lib

--- src/components/coffee-end/CoffeeEnd.js
+++ src/components/coffee-end/CoffeeEnd.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { AndroidBackHandler } from 'react-navigation-backhandler';
 import styles from './styles';
+
+const onBackPressed = (navigation) => {
+  navigation.navigate('CoffeeStart');
+}
 
 const CoffeeEnd = ({navigation}) => {
 
@@ -8,35 +13,36 @@ const CoffeeEnd = ({navigation}) => {
   const beverageType = 'Café Preto';
 
   return(
-    <View style={{flex:1, justifyContent: 'space-around'}}>
-      <View>
-        <Text style={[styles.header, styles.header_3]}>
-          Solicitação feita com sucesso!
-        </Text>
-        <Image  
-          resizeMode="contain"
-          source={require('../../../assets/images/checked.png')}
-          style={styles.done_img}
-        />
+    <AndroidBackHandler onBackPressed={()=>onBackPressed(navigation)}>
+      <View style={{flex:1, justifyContent: 'space-around'}}>
+        <View>
+          <Text style={[styles.header, styles.header_3]}>
+            Solicitação feita com sucesso!
+          </Text>
+          <Image  
+            resizeMode="contain"
+            source={require('../../../assets/images/checked.png')}
+            style={styles.done_img}
+          />
+        </View>
+
+        <View>
+          
+          <Text style={[styles.header, styles.header_1]}>
+            {liters}L
+          </Text>
+          <Text style={[styles.header, styles.header_3]}>
+            {beverageType}
+          </Text>
+        </View>
+
+        <TouchableOpacity 
+          onPress={() => navigation.navigate('CoffeeStart')}
+          style={styles.btn}>
+          <Text>Concluir</Text>
+        </TouchableOpacity>
       </View>
-
-      <View>
-        
-        <Text style={[styles.header, styles.header_1]}>
-          {liters}L
-        </Text>
-        <Text style={[styles.header, styles.header_3]}>
-          {beverageType}
-        </Text>
-      </View>
-
-      <TouchableOpacity 
-        onPress={() => navigation.navigate('CoffeeStart')}
-        style={styles.btn}>
-        <Text>Concluir</Text>
-      </TouchableOpacity>
-
-    </View>
+    </AndroidBackHandler>
   )
 }
 
