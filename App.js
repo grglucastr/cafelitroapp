e37ef31b4a3cc40a329235757059820a794a845f
George Bentes <george.bentes@gmail.com>
2019-08-14 21:39:26 -0400

    Change navigation stack

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
 import { 
   createStackNavigator, 
   createAppContainer 
 } from 'react-navigation';
+
 import CoffeeSelect from './src/components/coffee-select';
 import UserIdentification from './src/components/user-identification';
 import LitersSelect from './src/components/liters-select';
 import CoffeeEnd from './src/components/coffee-end';
 
-
 const App = createStackNavigator({
-  Home: LitersSelect,
+  CoffeeStart: {
+    screen: UserIdentification,
+    navigationOptions: {
+      header:null,
+    }
+  },
   CoffeeSelect,
   LitersSelect,
-  CoffeeEnd
+
+  CoffeeEnd:{
+    screen: CoffeeEnd,
+    navigationOptions: {
+      header: null,
+    },
+  },
 })
 
 export default createAppContainer(App);
